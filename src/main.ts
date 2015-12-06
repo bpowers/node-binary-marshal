e22cd181a63a48b2a7ab1ea3c7d87269ef3dcaaa
Bobby Powers <bobbypowers@gmail.com>
2015-12-05 22:53:59 -0500

    add main.ts for single entrypoint

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+// Copyright 2015 Bobby Powers. All rights reserved.
+// Use of this source code is governed by the ISC
+// license that can be found in the LICENSE file.
+
+'use strict';
+
+import * as m from './marshal';
+import * as s from './socket';
+
+export var Marshal = m.Marshal;
+//export var Unmarshal = m.Unmarshal;
+export var socket = s;
